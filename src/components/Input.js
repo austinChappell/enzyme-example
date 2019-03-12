a44a77908610d023294be77f77d5cbd992fa2403
austinChappell <Austin.W.Chappell@gmail.com>
2019-03-11 20:38:59 -0500

    clean up Input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,6 @@
 // External Dependencies
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 // Internal Dependencies
@@ -21,38 +21,44 @@ const StyledInput = styled.input({
       1px -1px 4px ${COLORS.primaryLight},
       -1px 1px 4px ${COLORS.primaryLight}
     `,
-  }
+  },
 });
 
+// Local Variables
+const propTypes = {
+  ariaLabel: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.string.isRequired,
+};
+const defaultProps = {
+  placeholder: null,
+  name: null,
+  type: 'text',
+};
+
 // Component Definition
-class Input extends Component {
-  static propTypes = {
-    ariaLabel: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    placeholder: PropTypes.string,
-    type: PropTypes.string,
-    value: PropTypes.string.isRequired,
-  };
+const Input = ({
+  ariaLabel,
+  name,
+  onChange,
+  placeholder,
+  type,
+  value,
+}) => (
+  <StyledInput
+    aria-label={ariaLabel}
+    name={name}
+    onChange={onChange}
+    placeholder={placeholder}
+    type={type}
+    value={value}
+  />
+);
 
-  static defaultProps = {
-    placeholder: null,
-    name: null,
-    type: 'text',
-  };
-
-  render() {
-    return (
-      <StyledInput
-        aria-label={this.props.ariaLabel}
-        name={this.props.name}
-        onChange={this.props.onChange}
-        placeholder={this.props.placeholder}
-        type={this.props.type}
-        value={this.props.value}
-      />
-    );
-  }
-}
+Input.propTypes = propTypes;
+Input.defaultProps = defaultProps;
 
 export default Input;
